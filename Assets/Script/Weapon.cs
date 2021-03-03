91c5e57d992e355627462bed40439f977ab6d6d3
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-03-03 14:28:02 +0700

    Raycasting

---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPSCamera;
+    [SerializeField] float range = 100f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range);
+        Debug.Log("I hit " + hit.transform.name);
+    }
+}
