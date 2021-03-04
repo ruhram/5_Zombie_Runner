d6467ee1b2e6c27402e3b9936c01fc69b6694d90
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-03-04 17:56:57 +0700

    Change Field of View

---
+++ Assets/Script/WeaponZoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera FPScamera;
+    [SerializeField] float zoomOutFOV = 60f;
+    [SerializeField] float zoomInFOV = 20f;
+
+    bool zoomInToggle = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomInToggle == false)
+            {
+                zoomInToggle = true;
+                FPScamera.fieldOfView = zoomInFOV;
+            }
+            else
+            {
+                zoomInToggle = false;
+                FPScamera.fieldOfView = zoomOutFOV;
+            }
+        }
+    }
+
+}
