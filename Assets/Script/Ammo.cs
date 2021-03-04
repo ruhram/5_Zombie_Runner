b74318111b01f3057cd508c580625a3e9b73d5eb
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-03-04 21:34:50 +0700

    Make different Weapon

---
+++ Assets/Script/Ammo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField] int ammo = 20;
+
+    public int GetCurrentAmmo()
+    {
+        return ammo;
+    }
+
+    public void ReduceCurrentAmmo()
+    {
+        ammo--;
+    }
+}
