d37d0ee895ccb343e3aca88cf17b0d3a42a65faf
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-03-04 22:32:18 +0700

    Select Weapon

--- Assets/Script/WeaponSwitcher.cs
+++ Assets/Script/WeaponSwitcher.cs
@@ -11,6 +11,62 @@ public class WeaponSwitcher : MonoBehaviour
         SetWeaponActive();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScroolWheel();
+
+        if(previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessScroolWheel()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if(currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        }
+        if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                currentWeapon--;
+            }
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 2;
+        }
+    }
+
     private void SetWeaponActive()
     {
         int weaponIndex = 0;
@@ -30,9 +86,5 @@ public class WeaponSwitcher : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
